0c7d06cba146ceea4a04bf801fed869d271be817
ZoeWangW <13438165026@163.com>
2018-09-13 17:13:14 +0800

    更新数据
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/databasetest/MainActivity.java
+++ app/src/main/java/com/zoe/databasetest/MainActivity.java
@@ -48,5 +48,16 @@ public class MainActivity extends AppCompatActivity {
                 db.insert("Boook",null,values);//插入第二条数据
             }
         });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price",10.99);
+                db.update("Book",values,"name=?",new String[]{"The Da Vinci Code"});
+            }
+        });
     }
 }
