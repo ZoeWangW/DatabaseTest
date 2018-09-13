8f84b66bc9c01d4412107c7d143901d454fda434
ZoeWangW <13438165026@163.com>
2018-09-13 17:05:06 +0800

    像数据库里添加数据
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/databasetest/MainActivity.java
+++ app/src/main/java/com/zoe/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zoe.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name","The Da Vinci Code");//开始组装第一条数据
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.95);
+                db.insert("Boook", null, values);//插入第一条数据
+                values.clear();
+                values.put("name","The Lost Symbol");//开始组装第二条数据
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",19.95);
+                db.insert("Boook",null,values);//插入第二条数据
             }
         });
     }
